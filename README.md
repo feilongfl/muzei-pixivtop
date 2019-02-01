3ab84e355fcf6d090f2ba2b33ee7583a4fc7ce9c
feilong <feilongphone@gmail.com>
2019-02-01 10:14:18 +0800

    copy from muzei com.example.muzei.unsplash

---
+++ README.md
@@ -0,0 +1,20 @@
+Muzei Live Wallpaper
+====================
+
+<img src="http://api.muzei.co/static/hero.jpg" width="400" align="right" hspace="20">
+
+*A living museum for your Android home screen.*
+
+Muzei is a live wallpaper that gently refreshes your home screen each day with famous works of art. It also recedes into the background, blurring and dimming artwork to keep your icons and widgets in the spotlight. Simply double touch the wallpaper or open the Muzei app to enjoy and explore the artwork in its full glory.
+
+Alternatively, you can choose your favorite photos from your own gallery or other apps to use on your home screen. To keep your wallpaper fresh, Muzei will rotate through your favorite photos every few hours.
+
+<a href="https://play.google.com/store/apps/details?id=net.nurik.roman.muzei" target="_blank">
+<img src="https://play.google.com/intl/en_us/badges/images/generic/en-play-badge.png" alt="Get it on Google Play" height="90"/></a>
+
+Muzei API
+=========
+
+Muzei offers a simple API that allows you to build your own wallpaper source.
+
+**[Read the API docs](http://api.muzei.co)**&nbsp;&nbsp;•&nbsp;&nbsp;**[Check out the Unsplash example source](https://github.com/romannurik/muzei/tree/master/example-unsplash)**
---
+++ muzei-pixiv/src/main/java/com/example/muzei/unsplash/UnsplashExampleWorker.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.muzei.unsplash
+
+import android.content.Context
+import android.util.Log
+import androidx.core.net.toUri
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.example.muzei.unsplash.BuildConfig.PIXIV_TOP
+import com.google.android.apps.muzei.api.provider.Artwork
+import com.google.android.apps.muzei.api.provider.ProviderContract
+import java.io.IOException
+
+class UnsplashExampleWorker(
+        context: Context,
+        workerParams: WorkerParameters
+) : Worker(context, workerParams) {
+
+    companion object {
+        private const val TAG = "UnsplashExample"
+
+        internal fun enqueueLoad() {
+            val workManager = WorkManager.getInstance()
+            workManager.enqueue(OneTimeWorkRequestBuilder<UnsplashExampleWorker>()
+                    .setConstraints(Constraints.Builder()
+                            .setRequiredNetworkType(NetworkType.CONNECTED)
+                            .build())
+                    .build())
+        }
+    }
+
+    override fun doWork(): Result {
+        val photos = try {
+            UnsplashService.popularPhotos()
+        } catch (e: IOException) {
+            Log.w(TAG, "Error reading Unsplash response", e)
+            return Result.retry()
+        }
+
+        if (photos.isEmpty()) {
+            Log.w(TAG, "No photos returned from API.")
+            return Result.failure()
+        }
+
+        val providerClient = ProviderContract.getProviderClient(
+                applicationContext, PIXIV_TOP)
+        val attributionString = applicationContext.getString(R.string.attribution)
+        providerClient.addArtwork(photos.map { photo ->
+            Artwork().apply {
+                token = photo.id
+                title = photo.description ?: attributionString
+                byline = photo.user.name
+                attribution = if (photo.description != null) attributionString else null
+                persistentUri = photo.urls.full.toUri()
+                webUri = photo.links.webUri
+                metadata = photo.user.links.webUri.toString()
+            }
+        })
+        return Result.success()
+    }
+}
